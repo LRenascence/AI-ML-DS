54880fb0480dec42420a07cb4d1b84ecb1be9916
Renascence <im.renascence@gmail.com>
2019-03-29 23:22:55 -0500

    updated ch5

--- Hands-onDSAndML/ch5MachineLearningWithPython/spamClassifierWithNaiveBayes.py
+++ Hands-onDSAndML/ch5MachineLearningWithPython/spamClassifierWithNaiveBayes.py
@@ -1,21 +1,7 @@
 import pandas as pd
 
+# read the data from csv file
 df = pd.read_csv('spam.csv', encoding = "ISO-8859-1")
-print(type(df))
-print(df.head())
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
+
+#
 print()

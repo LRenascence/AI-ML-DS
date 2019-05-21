0dfcf361c2163b5e73cff4355bb8243115b5ceab
Renascence <im.renascence@gmail.com>
2019-05-21 15:48:52 -0500

    updated ch5

--- Hands-onDSAndML/ch5MachineLearningWithPython/spamClassifierWithNaiveBayes.py
+++ Hands-onDSAndML/ch5MachineLearningWithPython/spamClassifierWithNaiveBayes.py
@@ -1,15 +1,34 @@
 import pandas as pd
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 # read the data from csv file
 df = pd.read_csv('spam.csv', encoding = "ISO-8859-1")
 
-#
 
-print()
-print()
-print()
-print()
-print()
-print()
-print()
-print()
+train = df[:5000]
+test = df[5001:]
+
+
+
+# counts is the word list and number of times
+vectorizer = CountVectorizer()
+counts = vectorizer.fit_transform(train.v2)
+# target is spam or ham
+classifier = MultinomialNB()
+targets = train.v1
+classifier.fit(counts, targets)
+
+# run on test set
+test_counts = vectorizer.transform(test.v2)
+predictions = classifier.predict(test_counts)
+
+
+match = 0
+result = test['v1'].values
+
+for i in range(len(predictions)):
+    if predictions[i] == result[i]:
+        match += 1
+
+print(match)
+print(match / len(predictions))

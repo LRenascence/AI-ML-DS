228e34c1bad9084ac17e567c2464a2445417d30d
Renascence <im.renascence@gmail.com>
2019-06-15 23:17:10 -0500

    updated ch5

--- Hands-onDSAndML/ch5MachineLearningWithPython/spamNavieBayes.py
+++ Hands-onDSAndML/ch5MachineLearningWithPython/spamNavieBayes.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+
+
 # read the data from csv file
 df = pd.read_csv('spam.csv', encoding = "ISO-8859-1")
 
